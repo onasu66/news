16543f0fd022f9cb17a4aa648e06a83da03eca99
unknown <0v09979@gmail.com>
2026-03-25 20:24:33 +0900

    render

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Render は PORT を設定する。未設定時は 10000（Render のデフォルト）で待ち受け
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000} --proxy-headers --forwarded-allow-ips='*'
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
